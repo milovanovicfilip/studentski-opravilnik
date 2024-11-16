d102fef6a1bb5191839aaf1f753536e75e7b37b8
milovanovicfilip <milovanovic8filip@gmail.com>
2024-11-16 22:05:42 +0100

    changed index for dev

--- server/index.mjs
+++ server/index.mjs
@@ -3,6 +3,7 @@ import express from 'express'
 import mongoose from "mongoose";
 import path from 'path'
 import cors from 'cors'
+import {taskRouter} from './Routers/Task.Router.mjs'
 import { fileURLToPath } from 'url'
 
 dotenv.config()
@@ -13,15 +14,17 @@ const PORT = process.env.PORT;
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname, "..", "client", "public")));
-app.use(cors({
+//app.use(express.static(path.join(__dirname, "..", "client", "public")));
+/*app.use(cors({
     origin: 'https://localhost:8080'
-}))
+}))*/
+app.use('/task',taskRouter)
 
 mongoose.connect(process.env.MONGO_URL, () => console.log("Connected to DB."));
 
 app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, "..", "client", "public", 'index.html'))
+    //res.sendFile(path.resolve(__dirname, "..", "client", "public", 'index.html'))
+    res.send();
 })
 
 app.listen(PORT,()=>{

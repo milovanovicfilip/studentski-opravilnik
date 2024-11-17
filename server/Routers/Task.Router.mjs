11925bb3abc3ebb3f5f0ec20d00a06919df84d7f
Filip Potočnik <github.dev@filpc.com>
2024-11-17 16:46:46 +0100

    Delete server/Routers directory

--- server/Routers/Task.Router.mjs
+++
@@ -1,9 +0,0 @@
-import express, {Router} from 'express';
-import TaskController from '../Controllers/Task.Controller.mjs';
-
-const router = express.Router();
-const taskController = new TaskController();
-
-router.get('/',taskController.getAllTasks);
-
-export const taskRouter = router;

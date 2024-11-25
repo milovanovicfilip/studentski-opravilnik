a25c241296151cb8c46bf5a363539f7df3ccfa40
milovanovicfilip <milovanovic8filip@gmail.com>
2024-11-25 09:33:06 +0100

    add user router

---
+++ server/Routes/User.Router.mjs
@@ -0,0 +1,19 @@
+import express, {Router} from 'express';
+import UserController from '../Controllers/User.Controller.js';
+import { authoriseUser } from '../utils/jwt.js';
+
+const router = express.Router();
+const userController = new UserController();
+
+router.post('/register',userController.addUser);
+router.post('/login',userController.loginUser);
+
+router.get('/logout',authoriseUser,userController.logoutUser);
+router.get('/getTasks/:id',authoriseUser,userController.getUserPosts);
+router.get('/:id', userController.getUserData);
+
+router.delete('/:id',authoriseUser,userController.removeUser);
+
+router.put('/:id',authoriseUser,userController.updateProfile);
+
+export const userRouter = router;

fe7f3c074ac232ce456f2cd016c1ac24c9252249
milovanovicfilip <milovanovic8filip@gmail.com>
2024-12-05 23:31:49 +0100

    add importFromCsv and exportToCsv functions

--- server/Controllers/Task.Controller.mjs
+++ server/Controllers/Task.Controller.mjs
@@ -1,5 +1,10 @@
 import { Task } from "../Models/Task.Model.mjs";
+import multer from "multer"
+import fs from "fs"
+import { Parser } from "json2csv"
+import csvParser from "csv-parser";
 
+const upload = multer({ dest: "uploads/" });
 export default class TaskController {
   constructor() {}
 
@@ -91,6 +96,48 @@ export default class TaskController {
         message: "Failed to delete task",
         error: error.message,
       });
+    }
+  };
+
+  exportToCSV = async (request, response) => {
+    try{
+      const tasks = await Task.find({userId: request.user.id})
+
+      const formattedTasks = tasks.map(task => ({
+        _id: task._id,
+        title: task.title,
+        content: task.content,
+        status: task.status,
+        priority: task.priority,
+        dueDate: task.dueDate,
+        createdAt: task.createdAt,
+        tags: task.tags.join(';'),
+        user: task.user
+      }));
+
+      const fields = ["_id","title","content","status","priority","dueDate","createdAt","tags","user"]
+      const parser = new Parser({ fields })
+      const csv = parser.parse(formattedTasks);
+
+      response.header('Content-Type','text/csv');
+      response.attachment(`tasks_${Date.now()}_${request.user.id}`)
+      response.send(csv);
+    }catch(error){
+      response.status(500).json({
+        message: "Error with exporting to CSV",
+        error: error.message
+      })
+    }
+  };
+  
+  importFromCSV = async (request, response) => {
+    try{
+      //.si
+    }catch(error){
+      response.status(500).json({
+        message: "Error with importing from CSV",
+        error: error.message
+      })
     }
   };
 }

b2f6be1d6911a88523a8b6c73051168f18d1b647
Filip Potočnik <github.dev@filpc.com>
2025-01-19 12:38:00 +0100

    fixed import in project router

--- server/Routers/Project.Router.mjs
+++ server/Routers/Project.Router.mjs
@@ -1,15 +1,15 @@
 import express from "express";
 import ProjectController from "../Controllers/Project.Controller.mjs";
-import { authenticateUser } from "../utils/authenticateUser.js";
+import { authoriseUser } from "../utils/authoriseUser.js";
 
 const router = express.Router();
 const projectController = new ProjectController();
 
-router.post("/", authenticateUser, projectController.createProject);
-router.get("/", authenticateUser, projectController.getAllProjects);
-router.get("/:id", authenticateUser, projectController.getProjectById);
-router.put("/:id", authenticateUser, projectController.updateProject);
-router.delete("/:id", authenticateUser, projectController.deleteProject);
+router.post("/", authoriseUser, projectController.createProject);
+router.get("/", authoriseUser, projectController.getAllProjects);
+router.get("/:id", authoriseUser, projectController.getProjectById);
+router.put("/:id", authoriseUser, projectController.updateProject);
+router.delete("/:id", authoriseUser, projectController.deleteProject);
 
 router.post("/add-task", projectController.addTaskToProject); // Add task to project
 
